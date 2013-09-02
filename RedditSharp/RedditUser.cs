868a98aa45b4cd01d745f5a90d0ba14f7b37d26a
eyarham <eric.yarham@gmail.com>
2013-09-02 14:31:01 -0400

    Update RedditUser.cs
    Added SetFlair function

--- RedditSharp/RedditUser.cs
+++ RedditSharp/RedditUser.cs
@@ -10,6 +10,7 @@ namespace RedditSharp
         private const string CommentsUrl = "/user/{0}/comments.json";
         private const string LinksUrl = "/user/{0}/submitted.json";
         private const string SubscribedSubredditsUrl = "/subreddits/mine.json";
+        private const string SetFlairUrl = "/api/flair";
 
         public RedditUser(Reddit reddit, JToken json) : base(json)
         {
@@ -57,6 +58,28 @@ namespace RedditSharp
         public override string ToString()
         {
             return Name;
+        }
+        
+        public void SetFlair(string flairText, string flairClass, string subName)
+        {
+            if (Reddit.User == null)
+                throw new Exception("No user logged in.");
+
+            var request = Reddit.CreatePost(SetFlairUrl);
+            Reddit.WritePostBody(request.GetRequestStream(), new
+            {
+                api_type = "json",
+                r = subName,
+                css_class = flairClass,
+                name = Name,
+                text = flairText,
+                uh = Reddit.User.Modhash
+            });
+            var response = request.GetResponse();
+            var result = Reddit.GetResponseString(response.GetResponseStream());
+            var json = JToken.Parse(result);
+            if (!json["json"].ToString().Contains("\"errors\": []"))
+                throw new Exception("Error editing flair.");
         }
     }
 }
