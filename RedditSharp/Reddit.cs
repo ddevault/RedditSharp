d8b020d0eb09c72fda6743504029e519078bfe68
raytheon <daprank007@gmail.com>
2013-10-18 13:21:55 +0200

    make reddit class smaller by moving unix time stamp to separate class

--- RedditSharp/Reddit.cs
+++ RedditSharp/Reddit.cs
@@ -304,14 +304,6 @@ namespace RedditSharp
             stream.Close();
         }
 
-        protected internal static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
-        {
-            // Unix timestamp is seconds past epoch
-            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-            return dtDateTime;
-        }
-
         #endregion
     }
 }
