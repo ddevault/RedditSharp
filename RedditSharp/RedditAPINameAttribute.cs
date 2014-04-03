143cd66ea42e65166da58660dce14a21eec4aa57
Adam Hellberg <sharparam@sharparam.com>
2014-04-03 14:13:04 +0200

    Added attribute class and converted some classes to use it

---
+++ RedditSharp/RedditAPINameAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedditSharp
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class RedditAPINameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public RedditAPINameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
