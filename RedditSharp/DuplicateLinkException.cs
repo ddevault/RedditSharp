c5297b5a366b25f9e059631b3a61ccc01c7fa99b
Jim Culverwell <jculverwell@gmail.com>
2014-05-08 17:45:11 -0400

    Added Duplicate Link Exception

---
+++ RedditSharp/DuplicateLinkException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RedditSharp
+{
+    /// <summary>
+    /// Exception that gets thrown if you try and submit a duplicate link to a SubReddit
+    /// </summary>
+    public class DuplicateLinkException : RedditException
+    {
+        public DuplicateLinkException()
+        {
+        }
+
+        public DuplicateLinkException(string message)
+            : base(message)
+        {
+        }
+
+        public DuplicateLinkException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
