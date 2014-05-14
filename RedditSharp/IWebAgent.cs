e3abb7f18b2a5b7124227a838ceb95a1ca2b582d
Erok21 <eric.yarham@gmail.com>
2014-05-14 13:36:49 -0400

    Add Uri Methods

--- RedditSharp/IWebAgent.cs
+++ RedditSharp/IWebAgent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json.Linq;
@@ -9,12 +10,18 @@ namespace RedditSharp
         CookieContainer Cookies { get; set; }
         string AuthCookie { get; set; }
         string AccessToken { get; set; }
+        [Obsolete("The Uri implementation of this method is preferred.")]
         HttpWebRequest CreateRequest(string url, string method);
+        [Obsolete("The Uri implementation of this method is preferred.")]
         HttpWebRequest CreateGet(string url);
+        [Obsolete("The Uri implementation of this method is preferred.")]
         HttpWebRequest CreatePost(string url);
+        HttpWebRequest CreateRequest(Uri url, string method);
+        HttpWebRequest CreateGet(Uri url);
+        HttpWebRequest CreatePost(Uri url);
         string GetResponseString(Stream stream);
         void WritePostBody(Stream stream, object data, params string[] additionalFields);
-        JToken CreateAndExecuteRequest(string url);
+        JToken CreateAndExecuteRequest(Uri url);
         JToken ExecuteRequest(HttpWebRequest request);
     }
 }
