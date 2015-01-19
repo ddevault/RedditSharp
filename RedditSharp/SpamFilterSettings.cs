e200c7aec12702bf6ed8004789f8f25a598cea85
Tony Montana <js.pwns@gmail.com>
2015-01-18 19:41:24 -0500

    Adds support for altering spam filter strength.
    This also has the added benefit of resolving a long-standing bug that would cause SubredditSettings to erroneously alter spamfilter strength settings when calling UpdateSettings()

---
+++ RedditSharp/SpamFilterSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditSharp
+{
+    public class SpamFilterSettings
+    {
+        public SpamFilterStrength LinkPostStrength { get; set; }
+        public SpamFilterStrength SelfPostStrength { get; set; }
+        public SpamFilterStrength CommentStrength { get; set; }
+
+        public SpamFilterSettings()
+        {
+            LinkPostStrength = SpamFilterStrength.High;
+            SelfPostStrength = SpamFilterStrength.High;
+            CommentStrength = SpamFilterStrength.High;
+        }
+    }
+}
