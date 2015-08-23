bd4643db9be2e69e89b3e367c4fa1239c5ddebff
Anubhav Saini <iamanubhavsaini@gmail.com>
2015-08-23 05:57:13 +0530

    Unit tests on Authentication.

---
+++ UnitTestRedditSharp/TestAuthentication.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedditSharp;
+
+namespace UnitTestRedditSharp
+{
+    [TestClass]
+    public class TestAuthentication
+    {
+        [TestMethod]
+        [Timeout(15000)]
+        [ExpectedException(typeof(System.Security.Authentication.AuthenticationException))]
+        public void ShouldFailOnWrongCredentials()
+        {
+            Reddit reddit = new Reddit("test", "test");
+        }
+
+        [TestMethod]
+        [Timeout(15000)]
+        public void ShouldAuthenticateOnRightCredentials()
+        {
+            Reddit reddit = new Reddit(UnshareableSensitiveInfo.USER, UnshareableSensitiveInfo.PASS);
+            Assert.IsNotNull(reddit.User);
+            Assert.AreEqual(UnshareableSensitiveInfo.USER, reddit.User.FullName);
+        }
+    }
+}
