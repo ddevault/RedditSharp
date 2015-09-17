cf9c789aef87680d4133fdc750f11c01f1d8a9eb
CrustyJew <mattiprice520@gmail.com>
2015-09-17 16:54:30 -0500

    Add functionality to read and parse Moderator Toolbox Usernotes from the wiki page.

---
+++ RedditSharp/ToolBoxUserNotesException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditSharp
+{
+    class ToolBoxUserNotesException : Exception
+    {
+        public ToolBoxUserNotesException()
+        {
+        }
+
+        public ToolBoxUserNotesException(string message)
+            : base(message)
+        {
+        }
+
+        public ToolBoxUserNotesException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
